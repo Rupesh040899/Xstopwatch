385f1fa71f0a930e03198901db13179ee31858a6
Rupesh <rupesh.mali0408@gmail.com>
2025-04-27 18:00:01 +0530

    narco

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from "react";
+export default function App() {
+  const [time, setTime] = useState(0);
+  const [val, setVal] = useState(true);
+
+  const handleChange = () => {
+    setVal((prevVal) => !prevVal);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setVal(false);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (val) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [val]);
+
+  const formatedTime = (seconds) => {
+    const min = Math.floor(seconds / 60);
+    const secondsRemain = seconds % 60;
+    return `${min}:${secondsRemain < 10 ? "0" : ""}${secondsRemain}`;
+  };
+  return (
+    <div className="App">
+      <h1>Stopwatch </h1>
+      Time: {formatedTime(time)}
+      <br />
+      <button onClick={handleChange}>{val ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
